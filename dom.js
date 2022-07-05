db7e304df29ba28a6022743d7a63984083fcf572
Prasannadatta Kawadkar <95745461+Prasannadatta@users.noreply.github.com>
2022-07-05 15:14:45 +0530

    file upload

---
+++ dom.js
@@ -0,0 +1,17 @@
+function func1()
+{
+  let n = document.getElementById('name').value;
+  console.log("name= "+ n);
+  console.log("no= "+ document.getElementById('phone').value);
+  console.log("email= "+ document.getElementById('email').value);
+ console.log("onchange is triggered");
+}
+
+function func2()
+{
+  let n = document.getElementById('name').value;
+  console.log("name= "+ n);
+  console.log("no= "+ document.getElementById('phone').value);
+  console.log("email= "+ document.getElementById('email').value);
+ console.log("oninput is triggered");
+}
